a04885a8471cba30e559326d0f259a5bac83a4dd
vaishnavi sasane <vaishnavisasane3@gmail.com>
2019-07-01 17:31:52 +0530

    first commit

--- AsmArrowMoveDemo/ViewController.swift
+++ AsmArrowMoveDemo/ViewController.swift
@@ -9,9 +9,27 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var ImageViewArrow: UIImageView!
+    
     override func viewDidLoad() {
+        
+//        UIView.animate(withDuration: 1) {
+//            self.ImageViewArrow.frame.origin.y-=200
+//        }
+        
         super.viewDidLoad()
+        
+        let hover = CABasicAnimation(keyPath: "position")
+
+        hover.isAdditive = true
+        hover.fromValue = NSValue(cgPoint: CGPoint.zero)
+        hover.toValue = NSValue(cgPoint: CGPoint(x: 0.0, y: 50.0))
+        hover.autoreverses = true
+        hover.duration = 0.5
+        hover.repeatCount = Float.infinity
+        ImageViewArrow.layer.add(hover, forKey: "myHoverAnimation")
+        
         // Do any additional setup after loading the view.
     }
 
